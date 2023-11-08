e4cfdb0badd7db56868f275ac5306f40218753a1
jabel13 <jalcanta@oswego.edu>
2023-11-07 23:43:33 -0500

    all, status, and search endpoints

--- go.mod
+++ go.mod
@@ -3,8 +3,21 @@ module server-jalcanta
 go 1.21.1
 
 require (
+	github.com/aws/aws-sdk-go v1.47.3
+	github.com/go-playground/validator/v10 v10.16.0
 	github.com/gorilla/mux v1.8.0
 	github.com/jamespearly/loggly v0.0.0-20220929110912-1d839b0715fa
 )
 
-require github.com/TwiN/go-color v1.4.1 // indirect
+require (
+	github.com/TwiN/go-color v1.4.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"os"
 	"net/http"
-	"time"
 	"github.com/gorilla/mux"
 	"github.com/jamespearly/loggly"
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/dynamodb"
+    "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
+
+type Outcome struct {
+	Name  string `json:"name"`
+	Price string `json:"price"`
+}
+
+type Item struct {
+	ID       string    `json:"id"`
+	Key      string    `json:"key"`
+	Outcomes []Outcome `json:"outcomes"`
+}
 
 // Struct for the log entry
 type LogEntry struct {
@@ -16,54 +31,240 @@ type LogEntry struct {
 	Status   int    `json:"status"`
 }
 
+type SearchParams struct {
+    ID  string // ID is a string up to 100 characters
+    Key string // Key is a string up to 50 characters
+}
+
 // custom response writer to capture the HTTP status code
 type responseWriter struct {
 	http.ResponseWriter
 	status int
 }
 
+func allHandler(w http.ResponseWriter, req *http.Request) {
+    sess, err := session.NewSession(&aws.Config{
+        Region: aws.String(os.Getenv("AWS_DEFAULT_REGION")),
+    })
+    if err != nil {
+        log.Fatalf("Failed to create AWS session: %s", err)
+    }
+
+    // Create DynamoDB client
+    svc := dynamodb.New(sess)
+
+    // Define the Scan input parameters
+    params := &dynamodb.ScanInput{
+        TableName: aws.String("nba-odds-jalcanta"),
+    }
+
+	    // Perform the Scan operation and get the result
+    result, err := svc.Scan(params)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    // Define a slice of the 'Item' struct type to hold the unmarshalled results
+    var items []Item
+
+    // Unmarshal the results into the slice of 'Item' structs
+    err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &items)
+    if err != nil {
+        http.Error(w, "Failed to unmarshal DynamoDB scan items", http.StatusInternalServerError)
+        return
+    }
+
+	// Set the Content-Type header
+    w.Header().Set("Content-Type", "application/json")
+
+	    // Write the JSON response
+    err = json.NewEncoder(w).Encode(items)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+}
+
 
 // responds with the system's current time
 func statusHandler(w http.ResponseWriter, req *http.Request) {
-	response := map[string]string{
-		"system_time": time.Now().Format(time.RFC3339),
-	}
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(response)
+    // Define the table name
+    tableName := "nba-odds-jalcanta"
+
+	svc := getDynamoDBClient()
+
+	    // Define the input parameters for the DescribeTable operation
+    input := &dynamodb.DescribeTableInput{
+        TableName: aws.String(tableName),
+    }
+
+	    // Perform the DescribeTable operation
+    result, err := svc.DescribeTable(input)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+	    // Extract the count of items from the result
+    itemCount := *result.Table.ItemCount
+
+    // Set the response object
+    response := map[string]interface{}{
+        "table":       tableName,
+        "recordCount": itemCount,
+    }
+
+    // Set the Content-Type header
+    w.Header().Set("Content-Type", "application/json")
+
+    // Write the JSON response
+    w.WriteHeader(http.StatusOK)
+    err = json.NewEncoder(w).Encode(response)
+    if err != nil {
+        http.Error(w, "Failed to encode response to JSON", http.StatusInternalServerError)
+    }
 }
 
-// logs incoming requests using Loggly
-func loggingMiddleware(next http.Handler) http.Handler {
+func searchHandler(w http.ResponseWriter, req *http.Request) {
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+   svc := getDynamoDBClient()
 
-		res := &responseWriter{ResponseWriter: w}
+   // Get 'id' and 'key' from query parameters
+   id := req.URL.Query().Get("id")
+   key := req.URL.Query().Get("key")
 
-		next.ServeHTTP(res, r)
+   // Check if 'Key' contains numbers
+   for _, char := range key {
+	   if char >= '0' && char <= '9' {
+		   http.Error(w, "400 Bad Request", http.StatusBadRequest)
+		   return
+	   }
+   }
 
-    	// Initialize loggly tag and client
-    	var tag string = "loggly-agent"
-		client := loggly.New(tag)
+   // Variables to store the results and any potential error
+   var items []Item
+   var err error
 
-		// Create a log entry with details about the request
-		logEntry := LogEntry{
-			Method:   r.Method,
-			SourceIP: r.RemoteAddr,
-			Path:     r.URL.Path,
-			Status:   res.status,
-		}
+   // If 'id' is provided, perform a Query operation
+   if id != "" {
+	   exprAttrNames := map[string]*string{"#id": aws.String("id")}
+	   exprAttrValues := map[string]*dynamodb.AttributeValue{
+		   ":idValue": {S: aws.String(id)},
+	   }
+	   keyCondExpr := "#id = :idValue"
 
-		// Convert the log entry to a JSON string
-		logData, err := json.Marshal(logEntry)
-		if err != nil {
-			log.Printf("Failed to marshal logEntry: %v", err)
-			return
-		}
+	   // Add Key condition if 'key' is also provided
+	   if key != "" {
+		   exprAttrNames["#key"] = aws.String("key")
+		   exprAttrValues[":keyValue"] = &dynamodb.AttributeValue{S: aws.String(key)}
+		   keyCondExpr += " AND #key = :keyValue"
+	   }
 
-		// Send the log to loggly
-		client.Send("info", string(logData))
-	})
+	   queryInput := &dynamodb.QueryInput{
+		   TableName:                 aws.String("nba-odds-jalcanta"),
+		   ExpressionAttributeNames:  exprAttrNames,
+		   ExpressionAttributeValues: exprAttrValues,
+		   KeyConditionExpression:    &keyCondExpr,
+	   }
+
+	   // Perform the query operation on DynamoDB
+	   var queryResult *dynamodb.QueryOutput
+	   queryResult, err := svc.Query(queryInput)
+	   if err != nil {
+		   http.Error(w, err.Error(), http.StatusInternalServerError)
+		   return
+	   }
+
+	   // If the query returns no results, return a 404 Not Found
+	   if len(queryResult.Items) == 0 {
+		   http.Error(w, "404 Page Not Found", http.StatusNotFound)
+		   return
+	   }
+
+	   // Unmarshal the result set into items
+	   if unmarshalErr := dynamodbattribute.UnmarshalListOfMaps(queryResult.Items, &items); unmarshalErr != nil {
+		   http.Error(w, unmarshalErr.Error(), http.StatusInternalServerError)
+		   return
+	   }
+
+	   w.Header().Set("Content-Type", "application/json")
+	   w.WriteHeader(http.StatusOK)
+
+    } else if key != "" {
+        // If only 'key' is provided, perform a Scan operation
+        scanInput := &dynamodb.ScanInput{
+            TableName: aws.String("nba-odds-jalcanta"),
+            FilterExpression: aws.String("#key = :keyValue"),
+            ExpressionAttributeNames: map[string]*string{"#key": aws.String("key")},
+            ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{":keyValue": {S: aws.String(key)}},
+        }
+
+        // Perform the scan operation on DynamoDB
+        var scanResult *dynamodb.ScanOutput
+        scanResult, err = svc.Scan(scanInput)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+		// Check if the scan returns no results and return a 404 Not Found if true
+        if len(scanResult.Items) == 0 {
+            http.Error(w, "404 Page Not Found", http.StatusNotFound)
+            return
+        }
+
+        if unmarshalErr := dynamodbattribute.UnmarshalListOfMaps(scanResult.Items, &items); unmarshalErr != nil {
+            http.Error(w, unmarshalErr.Error(), http.StatusInternalServerError)
+            return
+        }
+		
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+    } else {
+        // If neither 'id' nor 'key' is provided, return an error message
+        http.Error(w, "400 Bad Request", http.StatusBadRequest)
+        return
+    }
+
+    // Encode items to JSON and handle errors
+    if err := json.NewEncoder(w).Encode(items); err != nil {
+        http.Error(w, "Failed to encode items to JSON", http.StatusInternalServerError)
+    }
 }
+
+
+
+func catchAll(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        res := &responseWriter{ResponseWriter: w} // Initialize the custom response writer to capture the status code
+        defer func() {
+            if err := recover(); err != nil {
+                log.Printf("Recovered from a panic: %v", err)
+                http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+            }
+            // Log the request and response after the handler has finished executing
+            logEntry := LogEntry{
+                Method:   r.Method,
+                SourceIP: r.RemoteAddr,
+                Path:     r.URL.Path,
+                Status:   res.status,
+            }
+            logData, err := json.Marshal(logEntry)
+            if err != nil {
+                log.Printf("Failed to marshal logEntry: %v", err)
+            } else {
+                var tag string = "loggly-agent"
+                client := loggly.New(tag)
+                client.Send("info", string(logData))
+            }
+        }()
+        next.ServeHTTP(res, r) // Call the next handler
+    })
+}
+
 
 // // WriteHeader captures the status code before writing it
 func (rw *responseWriter) WriteHeader(status int) {
@@ -71,15 +272,36 @@ func (rw *responseWriter) WriteHeader(status int) {
 	rw.ResponseWriter.WriteHeader(status)
 }
 
+func getDynamoDBClient() *dynamodb.DynamoDB {
+    if awsSession == nil {
+		var err error
+		awsSession, err = session.NewSession(&aws.Config{
+			Region: aws.String(os.Getenv("AWS_DEFAULT_REGION")),
+		})
+		if err != nil {
+			log.Fatalf("Failed to create AWS session: %s", err)
+		}
+	}
+    return dynamodb.New(awsSession)
+}
+
+var awsSession *session.Session
+
 func main() {
+
 	// Set up the router
 	r := mux.NewRouter()
 	
-	// Define the route for "/status"
-	r.HandleFunc("/status", statusHandler).Methods("GET")
+	// Route for all endpoint
+	r.HandleFunc("/jalcanta/all", allHandler).Methods("GET")
 
-	// Add the middleware
-	r.Use(loggingMiddleware)
+	// Define the route for "jalcanta/status"
+	r.HandleFunc("/jalcanta/status", statusHandler).Methods("GET")
+
+	// Define the route for the search endpoint
+	r.HandleFunc("/jalcanta/search", searchHandler).Methods("GET")
+
+	r.Use(catchAll)
 
 	// Start server on port 8080
 	log.Fatal(http.ListenAndServe(":8080", r))
